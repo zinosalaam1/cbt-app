3df34744418cd505b4c9d2d50867177639ba13bf
Salaam Bola <bolatitosalaam7@gmail.com>
2025-03-24 00:45:36 -1100

    firt

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -13,6 +13,9 @@ from os import environ
 
 from pathlib import Path
 
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
