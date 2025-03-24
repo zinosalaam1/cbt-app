8f5e3556ec1307839cc5075229b1f53c8dd5e79f
Salaam Bola <bolatitosalaam7@gmail.com>
2025-03-24 00:59:13 -1100

    firt

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from os import environ
 
+import os
 from pathlib import Path
 
 # Import dj-database-url at the beginning of the file.
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
